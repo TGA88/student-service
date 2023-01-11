cf4f17701e351fd3446e3c6592c21dd5e172b657
yannapat <yannapat.yol@axonstech.com>
2023-01-11 10:34:49 +0700

    feat(upload-img-multer) : add img&body

--- apps/student-service-fastify/src/app/modules/user/route.ts
+++ apps/student-service-fastify/src/app/modules/user/route.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from "fastify";
 
-import { registerUser } from "./controller";
+import { getUser,createUser,deleteUser, login } from "./controller";
 
 async function userRoutes(server:FastifyInstance){
-   
-    server.post("/",registerUser)
+    server.get("/",getUser)
+    server.post("/login",login)
+    server.post("/create",createUser)
+    server.delete("/delete",deleteUser)
 }
      
 
